db52a328ccd2417cee8c671dcec0e5857135cecf
Jesper <reg+gitlab@jesperpetersen.se>
2020-12-19 16:11:38 +0100

    spice up HTML

--- src/App.svelte
+++ src/App.svelte
@@ -4,5 +4,5 @@
 
 
 <main class="flex items-center justify-center h-96">
-	<h1 class="text-3xl bg-red-300">{greeting}</h1>
+	<h1 class="p-10 text-3xl tracking-widest underline bg-red-300 rounded-md animate-pulse">{greeting}</h1>
 </main>
